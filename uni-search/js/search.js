593056a48a5b83ec4aa66858a4404092cccda59a
Yael <yael@yariv-HP-Pavilion-dv6500-Notebook-PC.(none)>
2013-07-16 19:22:28 +0300

    uni-search project added

---
+++ uni-search/js/search.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+getdata();
+})
+
+$('#search').keyup(function(){
+getdata();
+});
+
+function getdata(){
+var search_term = $('#search').val();
+$.ajax({
+dataType:'jsonp',
+url:  "http://nerdeez-server-dev.herokuapp.com/api/v1/university/?format=jsonp",
+success: function(data) {
+	$('#search_results').html(text);
+	var len = data.objects.length;
+	var text ='';
+    for(var i=0;i<len;i++){
+     	var obj= data.objects[i].description;
+     	if(obj.toLowerCase().contains(search_term.toLowerCase()))
+     	{
+            if (!(data.objects[i].image==null)){
+                text += "<div class='row'><div class='span4 result_item'>"
+                        +'<img width="200px" src="'+data.objects[i].image+'" />'
+                        +"</div></div>";
+                }
+            else{
+                text += "<div class='row'><div class='span4 result_item'>"
+                        +obj+"</div></div>";
+                }
+     	}
+     }
+       $('#search_results').html(text);
+    },
+error: function(data, temp){
+	console.log('Error');
+}
+})
+}
